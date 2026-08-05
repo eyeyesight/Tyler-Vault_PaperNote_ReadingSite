dcee98f98c045db45d7e320ff90198e28cf79dd8
Arke <arke@local.invalid>
2026-08-05 13:31:34 +0800

    release: first formal Pages candidate

---
+++ site/postscript-f491e311.js
@@ -0,0 +1,6 @@
+await Promise.all([
+  import("./static/scripts/script-0-f7c52b6a.js"),
+  import("./static/scripts/script-1-cc8afa9d.js"),
+  import("./static/scripts/script-2-7e107d7e.js")
+]);
+await import("./static/scripts/script-3-51ee2d22.js");
